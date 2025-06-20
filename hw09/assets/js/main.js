85dcface108fe1a6c83b42cfeefbbeeaeeafc0c6
Dunai Adrián <dunaiadrian1113@gmail.com>
2025-06-20 10:55:18 +0200

    scroll-top létrehozása

---
+++ hw09/assets/js/main.js
@@ -0,0 +1,24 @@
+(function () {
+    "use strict";
+
+    let scrollTop = document.querySelector('.scroll-top');
+
+    function toggleScrollTop() {
+        if (scrollTop) {
+            window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+        }
+    }
+
+    if (scrollTop) {
+        scrollTop.addEventListener('click', (e) => {
+            e.preventDefault();
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        });
+
+        window.addEventListener('load', toggleScrollTop);
+        document.addEventListener('scroll', toggleScrollTop);
+    }
+})();
